ad430bf813eeddd6bbb6b05105f60e3f073c87c6
Ramon Barros <contato@ramon-barros.com>
2016-04-20 12:00:08 -0300

    Add Email.

---
+++ src/Entity/Email.php
@@ -0,0 +1,95 @@
+<?php namespace Entity;
+
+use \UnexpectedValueException as Argument;
+use Illuminate\Container\Container;
+
+/**
+ * Classe para criação da entidade Pessoa Fisica
+ * @author Ramon Barros
+ * @package Pessoa
+ * @category cadastro
+ */
+class Email extends Container
+{
+    protected $type;
+
+    public function __construct($type = null, $address = null)
+    {
+        if (!empty($type) && !empty($address)) {
+            $this->setType($type)
+                 ->setAddress($address);
+        }
+    }
+
+    /**
+     * Registra o tipo do email
+     * @param string $type
+     */
+    public function setType($type = null)
+    {
+        if (is_null($type)) {
+            throw new Argument("Você deve informar o tipo do email.");
+        }
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Registra o endereço de email
+     * @param string $address
+     */
+    public function setAddress($address = null)
+    {
+        if (is_null($address)) {
+            throw new Argument("Você deve informar endereço do email.");
+        }
+        $this->instance("{$this->type}", $address);
+        return $this;
+    }
+
+    /**
+     * Recupera o numero telefonico referente ao tipo informado
+     * @param  string $type
+     * @return mixed
+     */
+    public function getAddress($type = null)
+    {
+        if (is_null($type)) {
+            throw new Argument("Antes você deve informar um tipo.");
+        }
+        $address = $this["{$type}"];
+        return $address;
+    }
+
+    /**
+     * Recupera as instancias dos numeros registrados
+     * @return object
+     */
+    public function getAddresses()
+    {
+        return $this->instances;
+    }
+
+    /**
+     * Dynamically access application services.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this[$key];
+    }
+
+    /**
+     * Dynamically set application services.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this[$key] = $value;
+    }
+}
---
+++ tests/Entity/EmailTest.php
@@ -0,0 +1,81 @@
+<?php namespace Entity;
+
+use Entity\AbstractTest;
+
+/**
+* Teste da entidade Telefone\Numero
+*
+* @category Tests
+*
+*/
+class EmailTest extends AbstractTest
+{
+    private $instance;
+
+    public function assertPreConditions()
+    {
+        $this->assertTrue(
+              class_exists($class = '\Entity\Email'),
+              'Class not found: '.$class
+        );
+        $this->instance = new \Entity\Email('email1', 'admin@admin.com');
+    }
+
+    public function testInstantiationWithoutArgumentsShouldWork()
+    {
+        $this->assertInstanceOf('\Entity\Email', $this->instance);
+    }
+
+    /**
+     * @depends testInstantiationWithoutArgumentsShouldWork
+     * @expectedException UnexpectedValueException
+     * @expectedExceptionMessage Você deve informar o tipo do email.
+     */
+    public function testSetTypeNull()
+    {
+        $this->instance->setType(null);
+    }
+
+    /**
+     * @depends testInstantiationWithoutArgumentsShouldWork
+     * @expectedException UnexpectedValueException
+     * @expectedExceptionMessage Você deve informar endereço do email.
+     */
+    public function testSetAddressNull()
+    {
+        $this->instance->setAddress(null);
+    }
+
+    /**
+     * @depends testInstantiationWithoutArgumentsShouldWork
+     * @expectedException UnexpectedValueException
+     * @expectedExceptionMessage Antes você deve informar um tipo.
+     */
+    public function testGetAddressTypeNull()
+    {
+        $this->instance->getAddress(null);
+    }
+
+    /**
+     * @depends testInstantiationWithoutArgumentsShouldWork
+     */
+    public function testGetAddress()
+    {
+        $this->instance
+             ->setType('email2')
+             ->setAddress('admin@admin.com.br');
+        $this->assertEquals( $this->instance->getAddress('email2') , 'admin@admin.com.br');
+        $this->assertEquals( $this->instance->email2, 'admin@admin.com.br' );
+
+        $this->assertEquals(
+            $this->instance->getAddresses(),
+            array(
+                'email1' => 'admin@admin.com',
+                'email2' => 'admin@admin.com.br'
+            )
+        );
+
+        $this->instance->email1 = 'user@user.com';
+        $this->assertEquals( $this->instance->getAddress('email1'), 'user@user.com');
+    }
+}
